fd10a7b11f866f24c4966dfc913f629c2bfd3119
ToshihiroOgino <tonigo0725@gmail.com>
2025-07-27 20:54:47 +0900

    docs: update README and tech report with implementation details and statistics

--- secure/util.go
+++ secure/util.go
@@ -7,20 +7,17 @@ import (
 )
 
 func escapeHTML(input string) template.HTML {
-	// First escape basic HTML characters
 	escaped := html.EscapeString(input)
 	return template.HTML(escaped)
 }
 
 func safeJSONString(input string) string {
-	// Escape backslashes and quotes
 	input = strings.ReplaceAll(input, "\\", "\\\\")
 	input = strings.ReplaceAll(input, "\"", "\\\"")
 	input = strings.ReplaceAll(input, "\n", "\\n")
 	input = strings.ReplaceAll(input, "\r", "\\r")
 	input = strings.ReplaceAll(input, "\t", "\\t")
 
-	// Escape HTML entities that could be dangerous in JSON context
 	input = strings.ReplaceAll(input, "<", "\\u003c")
 	input = strings.ReplaceAll(input, ">", "\\u003e")
 	input = strings.ReplaceAll(input, "&", "\\u0026")
